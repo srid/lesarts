fb4daea3605808ab4a81dcf2ada1489585fb966a
Sridhar Ratnakumar <srid@srid.ca>
2018-07-25 20:56:58 -0400

    cleanup

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -19,19 +19,19 @@ frontend = (head', body)
     head' = el "title" $ text "Les Arts"
     body = do
       el "h1" $ text "Les Arts"
-      demo
+      svgDemo
 
-demo :: MonadWidget t m => m ()
-demo = do
-  let dSVGEl = pure $ SVG_El (Width 600.0) (Height 400) Nothing
-  let attrs = mempty & at "id" ?~ "svg1" & at "class" ?~ "blue no-yellow"
-  let dRect1 = pure $ S.SVG_Rect
-          (S._PosX # 40.0)
-          (S._PosY # 40.0)
-          (S.Width 50.0)
-          (S.Height 50.0)
-          Nothing
-          Nothing
-  let shiftRect = fmap (S.svg_rect_pos_x . S._PosX +~ 3.0)
-  void $ svg_ dSVGEl $ do
-    S.svgBasicDyn S.Rectangle (mappend attrs . S.makeRectProps) (shiftRect dRect1) (pure mempty)
+svgDemo :: MonadWidget t m => m ()
+svgDemo = void $ svg_ dSVGEl $ do
+  S.svgBasicDyn S.Rectangle (mappend attrs . S.makeRectProps) (shiftRect dRect1) (pure mempty)
+  where
+    dSVGEl = pure $ SVG_El (Width 600.0) (Height 400) Nothing
+    attrs = mempty & at "id" ?~ "svg1" & at "class" ?~ "blue no-yellow"
+    dRect1 = pure $ S.SVG_Rect
+        (S._PosX # 40.0)
+        (S._PosY # 40.0)
+        (S.Width 50.0)
+        (S.Height 50.0)
+        Nothing
+        Nothing
+    shiftRect = fmap (S.svg_rect_pos_x . S._PosX +~ 3.0)
